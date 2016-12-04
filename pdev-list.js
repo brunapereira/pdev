ca504712a783b86097ebe6c28f51a15cb0bff195
Bruna <ppbruna2@gmail.com>
2016-12-04 16:38:08 -0200

    adds option to sort activity by pillar or createDate

--- pdev-list.js
+++ pdev-list.js
@@ -4,19 +4,36 @@ const program = require('commander')
 const fs = require('fs')
 const file = 'pdev.json'
 
-const printActivityInformation = (activity) => {
-  console.log(`Pillar: ${activity.pillar}`)
-  console.log(`Message: ${activity.message}`)
-  console.log(`date: ${activity.date}`)
-  console.log("")
+const read = (file) =>
+  (orderer) =>
+    fs.readFile(file, (error, actualData) => {
+      if (error) return console.log(error)
+      orderer(JSON.parse(actualData).activities)
+    })
+
+const printer = (activities) =>
+  activities.forEach((activity) => {
+    console.log(`Pillar: ${activity.pillar}`)
+    console.log(`Message: ${activity.message}`)
+    console.log(`date: ${activity.date}`)
+    console.log("")
+  })
+
+const orderByCreateDate = (activities) => {
+  printer(activities)
+}
+
+const orderByPillar = (activities) => {
+  const activitiesSortedByPillar = activities.sort((a, b) => {
+    if (a.pillar > b.pillar) { return 1; }
+    if (a.pillar < b.pillar) { return -1; }
+    return 0;
+  })
+
+  printer(activities)
 }
 
 program
+  .option('-p, --pillar', 'print information sorted by pillar', read(file)(orderByPillar))
+  .option('-a, --all', 'print all information', read(file)(orderByCreateDate))
   .parse(process.argv)
-
-fs.readFile(file, (error, actualData) => {
-  if (error) return console.log(error)
-
-  const actualDataObject = JSON.parse(actualData)
-  actualDataObject.activities.forEach(printActivityInformation)
-})
